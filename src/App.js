b8b54dec087007a02bcbc5687fa9ae3574a6b39a
= <naseersonu3@gmail.com>
2022-11-12 20:13:07 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 
 import './App.css';
 import Home from './Components/Home';
-//import StudentMymeeting from "./Components/StudeMymeeting";
-//import ViewAsses from "./Components/ViewAssess";
 
-//import './Styles/Scheduleclass.css';
-//import Scheduleclass from './Components/Scheduleclass';
-
-
-//import StudentProfile from './Components/StudentProfile';
 
 function App() {
 
 return (
    <div>
 <Home />
-
-{/* <StudentProfile/> */}
-{/* <StudentMymeeting/> */}
-{/* <ViewAsses/> */}
-{/* <Scheduleclass /> */}
-{/* <Courseadd /> */}
-{/* <StudentProfile/>
-<StudentMymeeting/>
-<ViewAsses/> */}
-
 </div>
 
 );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-// import Header from "../components/Header/Header";
-// import HeroSection from "../components/Hero-Section/HeroSection";
 import AboutUs from "../Components/AboutUs";
 import ChooseUs from "../Components/ChooseUs";
 import Courses from "../Components/Courses";
@@ -8,9 +6,6 @@ import Features from "../Components/Features";
 import Header from "../Components/Header";
 import HeroSection from "../Components/HeroSection";
 import Testimonials from "../Components/Testimonials";
-// import ChooseUs from "../components/Choose-us/ChooseUs";
-// import Features from "../components/Feature-section/Features";
-// import Testimonials from "../components/Testimonial/Testimonials";
 const Home = () => {
  return(
  <div> 

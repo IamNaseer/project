25642a49f0d461179e7972fc251b6509ba8a43a8
Tangudu sravani <85777679+Tangudu-Sravani@users.noreply.github.com>
2022-11-13 10:31:58 +0530

    courses updated

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -4,31 +4,24 @@ import "../Styles/courses.css";
 
 const coursesData = [
   {
-    id: "01",
+    
     title: "Web Design BootCamp-2022 for Beginners",
-    lesson: 12,
-    students: 12.5,
-    rating: 5.9,
+   
     // imgUrl: courseImg1,
    imgUrl: "/Images/web-design.png"
  
   },
 
   {
-    id: "02",
+   
     title: "Professional Graphics Design, PhotoShop, Adobe XD, Figma",
-    lesson: 12,
-    students: 12.5,
-    rating: 5.9,
+   
     imgUrl:"/Images/graphics-design.png" 
   },
 
   {
-    id: "03",
     title: "UI/UX BootCamp for Beginners in 2022",
-    lesson: 12,
-    students: 12.5,
-    rating: 5.9,
+   
     imgUrl:"/Images/ui-ux.png"
   },
 ];
